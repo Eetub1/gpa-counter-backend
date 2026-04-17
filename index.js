ed87652d54f93fa0dcfc18b65ab1352260ab67eb
Eetu Oinonen <eetu.oinonen04@gmail.com>
2026-04-17 19:26:55 +0300

    you can now edit courses

--- index.js
+++ index.js
@@ -30,6 +30,34 @@ app.get("/api/courses", async (req, res) => {
   }
 })
 
+app.put("/api/courses/:id", async (req, res) => {
+  const { id } = req.params
+  const newCourse = req.body
+
+  if (newCourse.password !== process.env.ADMIN_PASSWORD) {
+    return res.status(401).json({ 
+      error: "Unauthorized", 
+      message: "Invalid admin password" })
+  }
+
+  try {
+    const course = await Course.findOne({ _id: id })
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" })
+    }
+
+    course.name = newCourse.name || course.name
+    course.grade = newCourse.grade || course.grade
+    course.op = newCourse.op || course.op
+    course.description = newCourse.description || course.description
+
+    const updatedCourse = await course.save()
+    res.json(updatedCourse)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
 app.post("/api/courses", async (req, res) => {
   const { password, name, grade, op, description } = req.body
 
